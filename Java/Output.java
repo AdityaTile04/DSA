988c8f909a0e3962e211251e075dfed5edbb2d49
Aditya Tile <adityatile99@gmail.com>
2024-11-24 15:05:58 +0530

    learn output in java

---
+++ Java/Output.java
@@ -0,0 +1,7 @@
+public class Output {
+    public static void main(String args[]) {
+       System.out.print("Hello World!");
+       System.out.println("Hello World!"); // to add line space
+       System.out.print("Hello World \n"); // to add line space
+    }
+}
