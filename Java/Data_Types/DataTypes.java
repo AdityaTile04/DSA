6c9b78b7ec82ba25134f3b5e45dd19bc11102aea
Aditya Tile <adityatile99@gmail.com>
2024-11-26 20:14:08 +0530

    learn about variables

--- Java/Data_Types/DataTypes.java
+++ Java/Data_Types/DataTypes.java
@@ -2,6 +2,55 @@ package Data_Types;
 
 public class DataTypes {
     public static void main (String args[]) {
+        // Data types
+
+        // 1. byte
+        byte a = 127;
+        byte b = -128;
+        System.out.println(a);
+        System.out.println(b);
+
+        // 2. char
+        char ch1 = 'a';
+        char ch2 = 'b';
+        System.out.println(ch1);
+        System.out.println(ch2);
+
+        // 3. boolean
+        boolean one = true;
+        boolean two = false;
+        System.out.println(one);
+        System.out.println(two);
+
+        // 4. float
+        float pi = 3.14f;
+        float price = 10.5f;
+        System.out.println(pi);
+        System.out.println(price);
+
+        // 5.int
+        int n1 = 100;
+        int n2 = 2000000000;
+        System.out.println(n1);
+        System.out.println(n2);
+
+        // 6.double 
+        double piValue = 3.141592653589793;
+        double largeNum = 1.2345678950;
+        System.out.println(piValue);
+        System.out.println(largeNum);
+
+        // 7.long
+        long value = 1234567890;
+        long value2 = 12345678910L;
+        System.out.println(value);
+        System.out.println(value2);
+
+        // 8.short
+        short num = 120;
+        short num2 = 300;
+        System.out.println(num);
+        System.out.println(num2);
         
     }
 }
---
+++ Java/Data_Types/Sum.java
@@ -0,0 +1,10 @@
+package Data_Types;
+
+public class Sum {
+    public static void main(String main[]) {
+        int a = 10;
+        int b = 30;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
