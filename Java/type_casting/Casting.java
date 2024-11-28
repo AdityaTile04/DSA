97485130026d50321f191e1c9d627b17e6776c5e
Aditya Tile <adityatile99@gmail.com>
2024-11-28 12:57:41 +0530

    learn about type casting

---
+++ Java/type_casting/Casting.java
@@ -0,0 +1,15 @@
+package type_casting;
+
+public class Casting {
+    public static void main (String args[]) {
+        float a = 15.5f;
+        int b = (int) a;
+
+        System.out.println(b);
+
+        char ch = 'a';
+        int num = ch;
+        System.out.println(num);
+
+    }
+}
