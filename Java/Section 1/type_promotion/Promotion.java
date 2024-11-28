67f632177c7f0cfe8cd9473495cd4c9af391d413
Aditya Tile <adityatile99@gmail.com>
2024-11-28 13:39:04 +0530

    learn about type promotion in expression

---
+++ Java/Section 1/type_promotion/Promotion.java
@@ -0,0 +1,22 @@
+
+public class Promotion {
+    public static void main(String args[]) {
+        char a = 'a';
+        char b = 'b';
+        System.out.println((int) a);
+        System.out.println((int) b);
+        System.out.println(a+b);
+
+        byte  bt = 10;
+        short sh = 30;
+        char ch = 'a';
+        byte ans = (byte) (bt + sh + ch);
+        System.out.println(ans);
+
+        long lg = 50;
+        float fl = 30.4f;
+        double db = 20.3;
+        double sum = lg + fl + db;
+        System.out.println(sum);
+    }
+}
--- Java/output/Output.java
+++
@@ -1,8 +0,0 @@
-package output;
-public class Output {
-    public static void main(String args[]) {
-       System.out.print("Hello World!");
-       System.out.println("Hello World!"); // to add line space
-       System.out.print("Hello World \n"); // to add line space
-    }
-}
