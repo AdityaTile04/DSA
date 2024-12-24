4dbf52a1e781dbf682e7b2251e6bcd8f7cf357c3
Aditya Tile <adityatile99@gmail.com>
2024-12-24 12:45:17 +0530

    learn about arithmatic operators in java

--- Java/Section 2/operators/Arithmatic.java
+++ Java/Section 2/operators/Arithmatic.java
@@ -1,11 +1,11 @@
 public class Arithmatic {
-    public static void main (String args[]) {
-        int a = 10;
-        int b = 10;
-        System.out.println("add : " + (a+b));
-        System.out.println("subtract : " + (a-b));
-        System.out.println("multiply : " + (a*b));
-        System.out.println("divide : " + (a/b));
-        System.out.println("modulo : " + (a%b));
-    }
-}
+        public static void main (String args[]) {
+            int a = 10;
+            int b = 5;
+            System.out.println("Addition : " +  (a + b));
+            System.out.println("Subtract : " +  (a - b));
+            System.out.println("Multiply : " +  (a * b));
+            System.out.println("Divide : " +  (a / b));
+            System.out.println("Modulo : " +  (a % b));
+        }
+    }   
