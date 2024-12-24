cfd6b5031da8d3177666627807fec6ae636902ae
Aditya Tile <adityatile99@gmail.com>
2024-12-24 15:56:48 +0530

    learn about assignment operators

---
+++ Java/Section 2/operators/Assignment.java
@@ -0,0 +1,25 @@
+public class Assignment {
+    public static void main (String args[]) {
+        // Assigment operators
+
+        int a = 10;
+        a += 10; // a = a + 10
+        System.out.println(a);
+
+        int b = 20;
+        b -= 10; // b = b - 10
+        System.out.println(b);
+
+        int x = 20;
+        x *= 5; // x = x * 5
+        System.out.println(x);
+
+        int y = 40;
+        y /= 2; // y = y / 2
+        System.out.println(y);
+
+        int A = 5;
+        A %= 5; // A = A % 5
+        System.out.println(A); 
+    }
+}
