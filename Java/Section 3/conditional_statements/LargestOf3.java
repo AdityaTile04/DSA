b47f210df71ed0a8a625d5542716325f05f90ffa
Aditya Tile <adityatile99@gmail.com>
2024-12-25 23:11:27 +0530

    print largest of 3 numbers

---
+++ Java/Section 3/conditional_statements/LargestOf3.java
@@ -0,0 +1,13 @@
+public class LargestOf3 {
+    public static void main (String args[]) {
+        int a = 10, b = 8, c = 6;
+
+        if(a > b) {
+            System.out.println("A is big");
+        } else if (b > c) {
+            System.out.println("B is big");
+        } else {
+            System.out.println("C is big");
+        }
+     }
+}
