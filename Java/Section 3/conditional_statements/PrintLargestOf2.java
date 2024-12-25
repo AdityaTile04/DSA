412bbd343750f18d4b25e3b9c7cb29446f53cc93
Aditya Tile <adityatile99@gmail.com>
2024-12-25 20:25:49 +0530

    print largest of 2 numbers

---
+++ Java/Section 3/conditional_statements/PrintLargestOf2.java
@@ -0,0 +1,11 @@
+public class PrintLargestOf2 {
+    public static void main(String args[]) {
+        int a = 1, b = 3;
+
+        if (a > b) {
+            System.out.println("A is big");
+        } else {
+            System.out.println("B is big");
+        }
+    }
+}
