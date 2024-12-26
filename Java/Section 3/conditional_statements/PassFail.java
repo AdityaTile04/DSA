0809ea29ee27d8e0b2858c4dbdc7e2b899828d0f
Aditya Tile <adityatile99@gmail.com>
2024-12-26 11:12:51 +0530

    learn about switch statements in java

---
+++ Java/Section 3/conditional_statements/PassFail.java
@@ -0,0 +1,7 @@
+public class PassFail {
+    public static void main (String args[]) {
+        int marks = 27;
+        String result = (marks >= 28) ? "PASS" : "FAIL";
+        System.out.println(result);
+    }
+}
---
+++ Java/Section 3/conditional_statements/SwitchStatement.java
@@ -0,0 +1,21 @@
+public class SwitchStatement {
+    public static void main(String args[]) {
+
+        int num = 5;
+
+        switch (num) {
+            case 1:
+                System.out.println("FAN");
+                break;
+            case 2:
+                System.out.println("TV");
+                break;
+            case 3:
+                System.out.println("AC");
+                break;
+            default:
+                System.out.println("all swithes are off");
+        }
+
+    }
+}
