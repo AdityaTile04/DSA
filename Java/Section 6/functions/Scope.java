65dd3059893121ae890a08e833737a18f6bfc032
Aditya Tile <adityatile99@gmail.com>
2025-01-01 16:57:39 +0530

    learn about scope in java

---
+++ Java/Section 6/functions/Scope.java
@@ -0,0 +1,12 @@
+public class Scope {
+    public static void main(String args[]) {
+        int i = 20;
+
+        {
+            int number = 10; // block scope
+            System.out.println(number);
+        }
+        System.out.println(i);
+        // System.out.println(number); // can not use block scope variable in function scope
+    }
+}
