00e2d7510115f5bc6763a13c829f10e5154bb101
Aditya Tile <adityatile99@gmail.com>
2025-01-17 12:55:59 +0530

    Learned about inbuilt sorting functions and implemented examples for better understanding.

--- Sorting_Algorithms/InbuiltSort.java
+++ Sorting_Algorithms/InbuiltSort.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class InbuiltSort {
-
     public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
