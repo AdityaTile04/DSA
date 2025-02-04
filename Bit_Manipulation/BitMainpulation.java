964cd9f2c3de79e9275bdd66967284a9dda17cf1
Aditya Tile <adityatile99@gmail.com>
2025-02-04 15:19:31 +0530

    learn about binary XOR

--- Bit_Manipulation/BitMainpulation.java
+++ Bit_Manipulation/BitMainpulation.java
@@ -2,5 +2,8 @@ public class BitMainpulation  {
     public static void main(String args[]) {
         // Binary AND (&)
         System.out.println(5 & 6);
+
+        // Binary OR (|)
+        System.out.println(5 | 6);
     }
 }
