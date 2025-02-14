19f7fcc325f8e263bed885acdf6395993095e409
Aditya Tile <adityatile99@gmail.com>
2025-02-14 13:44:55 +0530

    learn about method overriding

--- oops/Polymorphism.java
+++ oops/Polymorphism.java
@@ -1,14 +1,17 @@
 public class Polymorphism {
     public static void main(String args[]) {
         Calculator a = new Calculator();
-        System.out.println(a.add(1000000, 500000));
+        System.out.println(a.add(10, 50));
         System.out.println(a.add(1000000, 10000000));
         System.out.println(a.add(1.5, 4.33));
+
+        Dog d = new Dog();
+        d.color();
     }
 }
 
 // types of polymorphism
-// 1. Method Overloading
+// 1. Method or Function Overloading
 class Calculator {
     int add(int a, int b) {
         return a + b;
@@ -20,5 +23,18 @@ class Calculator {
 
     float add(float a, float b) {
         return a + b;
+    }
+}
+// 2. Method or Function Overriding
+
+class Animal {
+    void color() {
+        System.out.println("black");
+    }
+}
+
+class Dog extends Animal {
+    void color() {
+        System.out.println("white");
     }
 }
