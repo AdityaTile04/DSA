ee285328ebe56b726ade8b8d0519279f59bbc151
Aditya Tile <adityatile99@gmail.com>
2025-02-18 15:26:54 +0530

    solve problem find last occurence of element in an array

---
+++ Recursion/recursion_part_one/LastOccurence.java
@@ -0,0 +1,20 @@
+public class LastOccurence {
+
+    public static int lastOccurence(int[] arr, int k, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+        int isFound = lastOccurence(arr, k, i + 1);
+
+        if (isFound == -1 && arr[i] == k) {
+            return i;
+        }
+
+        return isFound;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 8, 3, 6, 5, 5, 6, 4, 5 };
+        System.out.println(lastOccurence(arr, 5, 0));
+    }
+}
