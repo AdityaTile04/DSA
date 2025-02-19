118b4c3e3316c57051221579da424910a586fbb1
Aditya Tile <adityatile99@gmail.com>
2025-02-19 22:03:57 +0530

    optimized solution

--- Recursion/recursion_part_one/PowerOfN.java
+++ Recursion/recursion_part_one/PowerOfN.java
@@ -11,7 +11,23 @@ public class PowerOfN {
 
     }
 
+    public static int optimizedPower(int x, int n) {
+        if (n == 0)
+            return 1;
+
+        int halpPower = optimizedPower(x, n / 2);
+        int hpsq = halpPower * halpPower;
+
+        if (n % 2 != 0) {
+            hpsq = x * hpsq;
+        }
+
+        return hpsq;
+
+    }
+
     public static void main(String args[]) {
         System.out.println(power(5, 2));
+        System.out.println(optimizedPower(2, 5));
     }
 }
