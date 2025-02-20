c014da054eda654e8ac7b00b68a4738a4a3411ee
Aditya Tile <130766642+AdityaTile04@users.noreply.github.com>
2025-02-20 22:47:05 +0530

    Update PrintFibonacci.java

--- Recursion/recursion_part_one/PrintFibonacci.java
+++ Recursion/recursion_part_one/PrintFibonacci.java
@@ -3,14 +3,10 @@ public class PrintFibonacci {
     public static int fibonnaci(int n) {
         if (n == 0 || n == 1)
             return n;
-
         int fnmo = fibonnaci(n - 1);
         int fnmt = fibonnaci(n - 2);
-
         int fn = fnmo + fnmt; 
-
         return fn;
-
     }
 
     public static void main(String[] args) {
