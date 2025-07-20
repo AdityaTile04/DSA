29e274d26cfb5d644ff4fc4100ecc2761800123b
Aditya Tile <adityatile99@gmail.com>
2025-07-20 17:38:40 +0530

    learn about merge sort

---
+++ Divide_And_Conquer/MergeSort.java
@@ -0,0 +1,57 @@
+package Divide_And_Conquer;
+
+public class MergeSort {
+
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 9, 5, 2, 8, -1};
+        mergeSort(arr, 0, arr.length - 1);
+        printArr(arr);
+    }
+
+    public static void mergeSort(int[] arr, int start, int end) {
+        // Base case
+        if(start >= end) {
+            return;
+        }
+        int mid = start + (end - start) / 2;
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid + 1, end);
+        merge(arr, start, mid, end);
+    }
+
+    public static void merge(int[] arr, int start, int mid, int end) {
+        int[] temp = new int[end - start + 1];
+        int i = start;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= end) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        while (j <= end) {
+            temp[k++] = arr[j++];
+        }
+        // copy temo to original arr
+        for (k = 0, i = start; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+    }
+}
