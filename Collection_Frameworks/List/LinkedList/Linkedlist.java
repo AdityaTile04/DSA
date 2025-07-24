2d2d8c4b827504911d446ab12ceadf81f1c4982b
Aditya Tile <adityatile99@gmail.com>
2025-07-24 23:33:11 +0530

    learn operations in LL

--- Collection_Frameworks/List/LinkedList/Linkedlist.java
+++ Collection_Frameworks/List/LinkedList/Linkedlist.java
@@ -14,6 +14,7 @@ public class Linkedlist {
 
     public static Node head;
     public static Node tail;
+    public static int size;
 
     public void addFirst(int data) {
         Node newNode = new Node(data); // create new node
@@ -21,6 +22,7 @@ public class Linkedlist {
             head = tail = newNode;
             return;
         }
+        size++;
         // step 2 : create newNode next = head;
         newNode.next = head;
         // step 3: head => newNode;
@@ -32,6 +34,7 @@ public class Linkedlist {
         if (head == null) {
             head = tail = newNode;
         }
+        size++;
         tail.next = newNode;
         tail = newNode;
 
@@ -50,19 +53,86 @@ public class Linkedlist {
         System.out.println("null");
     }
 
+    public void add(int idx, int data) {
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+        size++;
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public int removeFirst() {
+        if (size == 0) {
+            System.out.println("LL is empty");
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    public int removeLast() {
+        if (size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MAX_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        Node prev = head;
+        for (int i = 0; i < size - 2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
+    public int search(int k) {
+        Node temp = head;
+        int i = 0;
+        while (temp != null) {
+            if (temp.data == k) { // key found
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1; // key not found
+    }
+
     public static void main(String[] args) {
         Linkedlist ll = new Linkedlist();
-        ll.printNode();
         ll.addFirst(2);
-        ll.printNode();
         ll.addFirst(1);
-        ll.printNode();
         ll.addLast(3);
-        ll.printNode();
-        ll.addLast(4);
-        ll.printNode();
-
+        ll.addLast(5);
+        ll.add(3, 4);
         ll.printNode();
 
+        // ll.removeFirst();
+        // ll.removeLast();
+        // System.out.println("Size of LL is: " + ll.size);
+
+        System.out.println("key found at index: " + ll.search(4));
+        System.out.println("key found at index: " + ll.search(10)); // not found return -1
     }
 }
