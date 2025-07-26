d8fd6b6656345df2bdbfa95a8fe31940cb3fe9ae
Aditya Tile <adityatile99@gmail.com>
2025-07-26 17:17:13 +0530

    solve LL problems

--- Collection_Frameworks/List/LinkedList/Linkedlist.java
+++ Collection_Frameworks/List/LinkedList/Linkedlist.java
@@ -215,6 +215,56 @@ public class Linkedlist {
         return true;
     }
 
+    public static boolean isCycle() {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void removeCycle() {
+        Node slow = head, fast = head;
+        boolean cycle = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                cycle = true;
+                break;
+            }
+        }
+        if (cycle == false) {
+            return;
+        }
+
+        slow = head;
+        Node prev = null;
+
+        // Special case: cycle starts at head
+        if (slow == fast) {
+            while (fast.next != slow) {
+                fast = fast.next;
+            }
+            fast.next = null;
+            return;
+        }
+
+        while (slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        prev.next = null;
+    }
+
     public static void main(String[] args) {
         Linkedlist ll = new Linkedlist();
         // ll.addFirst(2);
@@ -241,13 +291,23 @@ public class Linkedlist {
         // ll.printNode();
 
         // palindrome LL
-        ll.addLast(1);
-        ll.addLast(2);
-        ll.addLast(2);
+        // ll.addLast(1);
+        // ll.addLast(2);
+        // ll.addLast(2);
         // ll.addLast(1);
 
-        ll.printNode();
-        System.out.println(ll.checkPlindrome());
+        // ll.printNode();
+        // System.out.println(ll.checkPlindrome());
+
+        head = new Node(1);
+        Node temp = new Node(2);
+        head.next = temp;
+        head.next.next = new Node(3);
+        head.next.next.next = temp;
+
+        System.out.println(isCycle());
+        removeCycle();
+        System.out.println(isCycle());
 
     }
 }
